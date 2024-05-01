d0bcc4aa9316e24d0d5dd18249e2f7f1ba4ff424
Nana <nanalit16@gmail.com>
2024-05-01 16:11:45 +0300

    news page

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
 
 import sliderEngine from "./modules/slider";
+import video from "./modules/video";
 window.addEventListener("DOMContentLoaded", () => {
+    
+    const videoCardsButtons = document.querySelectorAll('.video-card__play')
+    const videoWrappers = document.querySelectorAll('.video-card__wrapper')
+    const videos = document.querySelectorAll('.video-card__wrapper video')
+
+    video(videoCardsButtons, videoWrappers, videos);
+
+
+
     const slider = document.querySelector('.slider .container')
     const slides = document.querySelectorAll('.slider__item');
     const total = document.querySelector('.counter__total');
@@ -23,4 +33,15 @@ window.addEventListener("DOMContentLoaded", () => {
     const indicatorsHero = document.createElement('ul');
 
     sliderEngine(sliderHero, slidesHero, totalHero, btnsHero, prevBtnHero, nextBtnHero, currentHero, indicatorsHero, 'activeHero', '.heroes__title p', ".heroes__descr span", ".heroes__button", ".heroes__heading p",".heroes__img img", 'heroes__item--active', 'scors__btn--active', 'dotsHero', 'dotsHero__item', 'dotsHero__item--active')
+
+    const sliderChronicle = document.querySelector('.chronic')
+    const slidesChronicle = document.querySelectorAll('.chronic__item');
+    const totalChronicle = document.querySelector('.numeric__total');
+    const btnsChronicle = document.querySelectorAll('.numeric__btn');
+    const prevBtnChronicle = document.querySelector('.numeric__dec');
+    const nextBtnChronicle = document.querySelector('.numeric__inc');
+    const currentChronicle = document.querySelector('.numeric__current');
+    const indicatorsChronicle = document.createElement('ul');
+
+    sliderEngine(sliderChronicle, slidesChronicle, totalChronicle, btnsChronicle, prevBtnChronicle, nextBtnChronicle, currentChronicle, indicatorsChronicle, 'activeChronicle', '.chronic__title span', ".chronic__descr span", ".chronic__button", ".chronic__heading p",".chronic__img img", 'chronic__item--active', 'numeric__btn--active', 'dotsChronic', 'dotsChronic__item', 'dotsChronic__item--active')
 })
